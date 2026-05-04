5e0c6ea58c270d1e0abf062867b99142d94105d3
shimo2829 <shimoda11492@outlook.jp>
2026-05-04 09:45:04 +0900

    Update HONOKA_Swimming_record.py

--- HONOKA_Swimming_record.py
+++ HONOKA_Swimming_record.py
@@ -28,7 +28,7 @@ st.markdown(
             top: 0;
             left: 0;
             width: 100%;
-            background-color: #FFC0CB;
+            background-color: #FFB6C1;
             padding: 20px 30px;
             border-bottom: 2px solid #ddd;
             z-index: 1000000;
